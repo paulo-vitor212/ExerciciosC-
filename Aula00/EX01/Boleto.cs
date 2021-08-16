2266dc63e7e6e5b27600f16989e006e3461f79b1
Paulo Vitor <paulovitor2123@gmail.com>
2021-08-16 20:43:35 -0300

    Aula00

---
+++ Aula00/EX01/Boleto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX01
+{
+    class Boleto
+    {
+        private string cedente;
+        private string descricao;
+        private double valorServico;
+        private DateTime dataEmissao = DateTime.Now;
+        private DateTime dataVencimento = DateTime.Now.Date.AddDays(7);
+        private DateTime dataPagamento;
+        private int diasAtrasados = 0;
+
+        public string Cedente { get => cedente; set => cedente = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        public double ValorServico { get => valorServico; set => valorServico = value; }
+        public DateTime DataEmissao { get => dataEmissao; set => dataEmissao = value; }
+        public DateTime DataVencimento { get => dataVencimento; set => dataVencimento = value; }
+        public DateTime DataPagamento { get => dataPagamento; set => dataPagamento = value; }
+        public int DiasAtrasados { get => diasAtrasados; set => diasAtrasados = value; }
+
+
+        public double Calcular()
+        {
+            double valor = ValorServico;
+
+            if(DataPagamento > DataVencimento)
+            {
+                //boleto com atraso
+                valor += 100;
+                TimeSpan dias = DataPagamento - DataVencimento;
+
+                if(dias.Days > 0)
+                {
+                    DiasAtrasados = dias.Days;
+                    var jurosDiarios = DiasAtrasados / 100f;
+                    valor = ValorServico * Convert.ToDouble(jurosDiarios) + valor;
+                }
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ Aula00/EX01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EX01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************Contas a Pagar***************\n\n");
+            Console.WriteLine("**************Calculador de Pagamento***************");
+
+            Boleto boleto = new Boleto();
+
+            Console.Write($"Digite o nome do Cedente: ");
+            boleto.Cedente = Console.ReadLine();
+            Console.Write($"Descrição do Serviço: ");
+            boleto.Descricao= Console.ReadLine();
+            Console.Write($"Valor do Serviço: ");
+            boleto.ValorServico = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("\n**************");
+            Console.WriteLine($"Cedente: {boleto.Cedente}");
+            Console.WriteLine($"Descrição do Serviço: {boleto.Descricao}");
+            Console.WriteLine($"Valor do Serviço: {boleto.ValorServico:C2}");
+            Console.WriteLine($"Data de Emissão: {boleto.DataEmissao}");
+            Console.WriteLine($"Data de Vencimento: {boleto.DataVencimento}");
+            Console.WriteLine("**************");
+
+            Console.Write("Digite a data do pagamento:");
+            boleto.DataPagamento = Convert.ToDateTime(Console.ReadLine());
+
+            double valor = boleto.Calcular();
+
+            Console.WriteLine($"Valor a Pagar: {valor:C2}");
+            if(boleto.DiasAtrasados > 0)
+            {
+                Console.WriteLine($"O boleto foi pago com {boleto.DiasAtrasados} de atraso");
+            }
+
+
+
+        }
+    }
+}
