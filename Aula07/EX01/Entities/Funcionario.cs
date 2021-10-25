3857610a5bb662782a65018f58d4fc24d3191aed
Paulo Vitor <paulovitor2123@gmail.com>
2021-10-25 01:59:26 -0300

    Lista 07

---
+++ Aula07/EX01/Entities/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX01.Entities
+{
+    public enum Genero
+    {
+        F,
+        M
+    }
+    class Funcionario : IComparable
+    {
+        private int id;
+        private string nome;
+        private int idade;
+        private Genero genero;
+
+        public Funcionario(int id, string nome, int idade, Genero genero)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.idade = idade;
+            this.genero = genero;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public Genero Genero { get => genero; set => genero = value; }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new ArgumentException("Erro de comparação");
+            }
+
+            Funcionario func = obj as Funcionario;
+
+            return idade.CompareTo(func.idade);
+        }
+    }
+}
---
+++ Aula07/EX01/Program.cs
@@ -0,0 +1,47 @@
+﻿using EX01.Entities;
+using EX01.Service;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EX01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Funcionario> list = new List<Funcionario>();
+
+            string origemArquivoPath = @"..\..\..\..\Funcionarios.txt";
+            try
+            {
+                string[] funcionarios = File.ReadAllLines(origemArquivoPath);
+
+                foreach (string funcionario in funcionarios)
+                {
+                    string[] arrayLine = funcionario.Split(",");
+                    int id = Convert.ToInt32(arrayLine[0]);
+                    string name = arrayLine[1];
+                    int idade = Convert.ToInt32(arrayLine[2]);
+                    Genero genero = arrayLine[3] == "F" ? Genero.F : Genero.M;
+
+                    Funcionario func = new Funcionario(id, name, idade, genero);
+    
+                    list.Add(func);
+                }
+
+                CalculationService calculation = new CalculationService();
+                Funcionario funcMaisVelho = calculation.Max(list);
+
+                Console.WriteLine($"O funcionário mais velho da empresa é o {funcMaisVelho.Nome} com {funcMaisVelho.Idade} de idade.");
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
